0d96607bea852e501e88c92d66ba63e18cb5c9ed
antonpobigai <antonpobigai@gmail.com>
2017-08-27 20:28:07 +0300

    Tab bar chenged

--- Primat Store/StickerController.swift
+++ Primat Store/StickerController.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+class Sticker {
+    
+    var name: String
+    var price: String?
+    var size: String
+    var label: String?
+    var logotip: URL?
+    var details: String
+    
+    init (name: String, price: String?, size: String, lable: String, logotip: URL?, details: String){
+        
+        self.name = name
+        self.price = price
+        self.size = size
+        self.label = lable
+        self.logotip = logotip
+        self.details = details
+        
+    }
+    
+}
